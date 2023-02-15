5fe50c68c7759e2f73804efd81178beadb7f8079
bhagwat-chate <bhagwat.chate25@gmail.com>
2023-02-15 20:48:28 +0530

    update s3 bucket

--- sensor/constant/s3_bucket.py
+++ sensor/constant/s3_bucket.py
@@ -1,2 +1,3 @@
-TRAINING_BUCKET_NAME = "sensor-bucket-pipeline-bhagwat-chate"
+TRAINING_BUCKET_NAME = "scania-sensor-pipeline-bhagwat-chate"
+# TRAINING_BUCKET_NAME = "sensor-bucket-pipeline-bhagwat-chate"
 PREDICTION_BUCKET_NAME = "sensor-datasource"
