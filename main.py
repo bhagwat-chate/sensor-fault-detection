b7683c79ad985dde0ece5035b2ccd471edaabdee
bhagwat-chate <bhagwat.chate25@gmail.com>
2023-03-01 10:46:52 +0530

    update

--- main.py
+++ main.py
@@ -88,8 +88,7 @@ def main():
         logging.exception(e)
 
 
-
 if __name__=="__main__":
-    # main()
+    main()
     # set_env_variable(env_file_path)
-    app_run(app, host=APP_HOST, port=APP_PORT)
+    # app_run(app, host=APP_HOST, port=APP_PORT)
